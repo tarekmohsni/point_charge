4579976001c118b25275e87648181e757237bb24
tarekmohsni <tarekmohsni100@gmail.com>
2024-02-14 12:33:48 +0100

    init projet

---
+++ index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+
+const app = express();
+const PORT = 2000;
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
+app.use(cors());
+
+
+app.listen(PORT,() => {
+
+    console.log(`Server is Successfully Running, and App is listening on port ${PORT}`);
+
+});
