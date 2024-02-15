406ffb3f7ee76f5989206e20ee953caabfdb8df9
tarekmohsni <tarekmohsni100@gmail.com>
2024-02-15 08:12:16 +0100

    creer point charge

--- src/objet_métier/point_chargeBo.ts
+++ src/objet_métier/point_chargeBo.ts
@@ -3,6 +3,8 @@ import { models } from '../models/database';
 import {generateIdPointCharge} from "../helpers/helpers";
 
 class PointChargeBo {
+
+    //creér point charge
     async createPointCharge (req:Request, res:Response){
         try {
 
@@ -20,15 +22,7 @@ class PointChargeBo {
                     message: "Le type d'emplacement doit être 'domicile' ou 'bureaux'." });
             }
 
-            let point_charge_id: string | null = null;
-
-            while (!point_charge_id) {
-                const currentId:string = generateIdPointCharge();
-                const existingPoint = await models.point_charge.findOne({ where: { point_charge_id: currentId } });
-                if (!existingPoint) {
-                    point_charge_id = currentId;
-                }
-            }
+            const point_charge_id = await this.generateUniquePointChargeId();
 
             const newChargingPoint = await models.point_charge.create({ point_charge_id, nom, type_emplacement });
 
@@ -41,6 +35,24 @@ class PointChargeBo {
             res.status(500).json({ message: 'Erreur lors de la création du point de charge.' });
         }
     }
+
+    async generateUniquePointChargeId(): Promise<string> {
+        try {
+            let init_point_charge_id: string | null = null;
+            while (!init_point_charge_id) {
+                const currentId: string = generateIdPointCharge();
+                const existingPoint = await models.point_charge.findOne({ where: { point_charge_id: currentId } });
+                if (!existingPoint) {
+                    init_point_charge_id = currentId;
+                }
+            }
+            return init_point_charge_id;
+        } catch (error) {
+            throw new Error('Une erreur est survenue lors de la génération de l identifiant unique du point de charge.');
+        }
+    }
+
+
 }
 
 export = PointChargeBo
